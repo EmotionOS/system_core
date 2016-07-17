20e6baa8152c2f8855102b66f06792a33ae2b3a5
mydongistiny <jaysonedson@gmail.com>
2016-07-16 17:22:55 -0700

    Revert "Add a trace constant for PackageManager"
    This reverts commit bea69676ebf62bde5a9e45dfc5c7aaf21dc45555.
    
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- include/cutils/trace.h
+++ include/cutils/trace.h
@@ -67,7 +67,10 @@ __BEGIN_DECLS
 #define ATRACE_TAG_RS               (1<<15)
 #define ATRACE_TAG_BIONIC           (1<<16)
 #define ATRACE_TAG_POWER            (1<<17)
-#define ATRACE_TAG_LAST             ATRACE_TAG_POWER
+#define ATRACE_TAG_PACKAGE_MANAGER  (1<<18)
+#define ATRACE_TAG_SYSTEM_SERVER    (1<<19)
+#define ATRACE_TAG_DATABASE         (1<<20)
+#define ATRACE_TAG_LAST             ATRACE_TAG_DATABASE
 
 // Reserved for initialization.
 #define ATRACE_TAG_NOT_READY        (1LL<<63)
